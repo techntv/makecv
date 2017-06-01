c8298c8f8a9c7991f9339df61f83b29fb6ac6f81
The Vinh <techntvz@gmail.com>
2017-06-01 16:37:07 -0700

    make gulp workflow

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,34 @@
 "use strict";
-import gulp from 'gulp';
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass   = require('gulp-sass'),
+      maps = require('gulp-sourcemaps');
 
 
+gulp.task('concatScripts', function(){
+    gulp.src(['asset/vendor/jquery.js', 'asset/vendor/foundation.min.js', 'asset/vendor/what-input.js', 'asset/js/app.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/js'));
+});
 
+gulp.task('minifyScripts', function() {
+    gulp.src("dist/js/main.js")
+    .pipe(uglify())
+    .pipe(rename('main.min.js'))
+    .pipe(gulp.dest("dist/js/"));
+})
 
-import metalsmith from 'gulp-metalsmith';
+gulp.task('compileSass', function(){
+    gulp.src('asset/scss/main.scss')
+    .pipe(maps.init())
+    .pipe(sass())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist/css/'))
+});
+gulp.task('default', ["concatScripts", "minifyScripts"], function() {
+    console.log("This is a default command");
+})
+
    
