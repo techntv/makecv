23c7b871f2d544fad4b8f42849419764576c9ece
The Vinh <techntvz@gmail.com>
2017-06-01 17:07:47 -0700

    gulp update build func

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,27 +8,29 @@ var gulp = require('gulp'),
 
 
 gulp.task('concatScripts', function(){
-    gulp.src(['asset/vendor/jquery.js', 'asset/vendor/foundation.min.js', 'asset/vendor/what-input.js', 'asset/js/app.js'])
+    return gulp.src(['asset/vendor/jquery.js', 'asset/vendor/foundation.min.js', 'asset/vendor/what-input.js', 'asset/js/app.js'])
+    .pipe(maps.init())
     .pipe(concat('main.js'))
+    .pipe(maps.write('./'))
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('minifyScripts', function() {
-    gulp.src("dist/js/main.js")
+gulp.task('minifyScripts', ["concatScripts"], function() {
+    return gulp.src("dist/js/main.js")
     .pipe(uglify())
     .pipe(rename('main.min.js'))
     .pipe(gulp.dest("dist/js/"));
-})
+});
 
 gulp.task('compileSass', function(){
-    gulp.src('asset/scss/main.scss')
+   return gulp.src('asset/scss/main.scss')
     .pipe(maps.init())
     .pipe(sass())
     .pipe(maps.write('./'))
     .pipe(gulp.dest('dist/css/'))
 });
-gulp.task('default', ["concatScripts", "minifyScripts"], function() {
-    console.log("This is a default command");
-})
+
+gulp.task("build", ["minifyScripts", "compileSass"]);
+gulp.task('default', ["build"]);
 
    
